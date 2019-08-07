aa1ba650ce65f08ef4583e0aba0b699bf38ef686
n8jhj <nathaniel.j.jones@wsu.edu>
2019-08-07 12:39:08 -0700

    Update worker.py
    Formatting.

--- soon_kyoo/worker.py
+++ soon_kyoo/worker.py
@@ -15,22 +15,23 @@ class Worker:
         worker = Worker(task=task)
         worker.start()
     """
-    def __init__(self, task) -> None:
+    def __init__(self, task):
         self.task = task
 
-    def start(self,):
+    def start(self):
         while True:
             try:
+                # Read database.
                 _dequeued_item = self.task.broker.dequeue(
                     queue_name=self.task.task_name)
                 dequeued_item = json.loads(_dequeued_item)
-                task_id = dequeued_item["task_id"]
-                task_args = dequeued_item["args"]
-                task_kwargs = dequeued_item["kwargs"]
-
-                print("running task: {0}".format(task_id))
+                task_id = dequeued_item['task_id']
+                task_args = dequeued_item['args']
+                task_kwargs = dequeued_item['kwargs']
+                # Do run.
+                print(f'Running task: {task_id}')
                 self.task.run(*task_args, **task_kwargs)
-                print("succesful run of task: {0}".format(task_id))
+                print(f'Succesful run of task: {task_id}')
             except Exception:
-                print("Unable to execute task.")
+                print(f'Unable to execute task {self.task}.')
                 continue       
