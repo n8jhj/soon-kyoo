e2b9b9e411352721414905c27426bbb6f7de6e88
n8jhj <nathaniel.j.jones@wsu.edu>
2019-08-06 18:34:47 -0700

    Import work from Komu W's article
    Nothing works yet, and some modifications have been made. Will be using SQLite instead of Redis.
    
    https://www.komu.engineer/blogs/celery-clone/understand-how-celery-works

---
+++ timer_tasks.py
@@ -0,0 +1,31 @@
+"""Script for running a timer task.
+"""
+
+import time
+
+from soon_kyoo.task import BaseTask
+
+
+class TimerTask(BaseTask):
+    """Task to count to a certain number a certain number of times.
+    """
+
+    task_name = "TimerTask"
+
+    def run(self, interval, n):
+        """Count at the given interval the given number of times.
+
+        Positional arguments:
+        interval - (int) Interval, in seconds.
+        n - (int) Number of times to count the given interval.
+        """
+        for i in range(n):
+            now = time.strftime()
+            print(f'{i}/{n} -- {now} -- Sleeping for {interval} seconds.')
+            time.sleep(interval)
+        print(f'Slept {interval * n} seconds total.')
+
+
+if __name__ == '__main__':
+    timer_task = TimerTask()
+    timer_task.run(5, 3)
---
+++ timer_worker.py
@@ -0,0 +1,13 @@
+"""Script that implements an e-commerce worker.
+"""
+
+from soon_kyoo.worker import Worker
+from timer_tasks import TimerTask
+
+
+if __name__ == "__main__":
+    timer_task = TimerTask()
+
+    # Run workers.
+    worker = Worker(task=timer_task)
+    worker.start()
