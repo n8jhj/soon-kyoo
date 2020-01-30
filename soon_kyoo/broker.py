bc440589e7d1e7fa3a8b19b7ecc30ef6006bca8b
n8jhj <n8jhj@live.com>
2020-01-30 13:08:37 -0800

    broker.py: Format SQLite statements

--- soon_kyoo/broker.py
+++ soon_kyoo/broker.py
@@ -18,7 +18,12 @@ class Broker:
         con = sqlite3.connect(str(db_path))
         with con:
             c = con.execute(
-                'SELECT position FROM queue ORDER BY position DESC')
+                '''
+                SELECT position
+                FROM queue
+                ORDER BY position DESC
+                '''
+            )
             max_position = c.fetchone()
             new_position = max_position[0] + 1 if max_position else 0
             con.execute('INSERT INTO queue VALUES (?, ?, ?, ?, ?, ?)', (
@@ -31,9 +36,20 @@ class Broker:
         con = sqlite3.connect(str(db_path))
         con.row_factory = sqlite3.Row
         with con:
-            c = con.execute(f'''SELECT * FROM queue
+            c = con.execute(
+                f'''
+                SELECT
+                    task_id,
+                    position,
+                    published,
+                    queue_name,
+                    args,
+                    kwargs
+                FROM queue
                 WHERE queue_name = {queue_name!r}
-                ORDER BY position ASC''')
+                ORDER BY position ASC
+                '''
+            )
             dequeued_item = c.fetchone()
             item_id = dequeued_item['task_id']
             c = con.execute(f'DELETE FROM queue WHERE task_id = {item_id!r}')
