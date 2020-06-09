dc4367c999f15d7606c8b0fe7fcd1639bef14342
n8jhj <nathaniel.j.jones@wsu.edu>
2020-06-09 12:05:06 -0700

    Move all timer functionality into timer_task.py

--- examples/timer_task.py
+++ examples/timer_task.py
@@ -5,7 +5,20 @@ import time
 
 import soon_kyoo as sk
 
-from timer_function import timer_sleep_all
+
+def timer_sleep_all(interval, n):
+    for i in range(n):
+        timer_sleep(interval, i, n)
+
+
+def timer_sleep(interval, i=None, n=None):
+    if None not in (i, n):
+        msg = f"{i+1}/{n} "
+    else:
+        msg = ''
+    msg += f"Sleeping {interval} seconds..."
+    sk.echo(msg)
+    time.sleep(interval)
 
 
 class TimerTask(sk.BaseTask):
